cbbe9fbe2fe35554cc6c638213f098d1fa86017f
kiah <kiah.f.han@newegg.com>
2017-09-22 14:54:50 -0700

    update SpringBootApplication on main class

--- src/main/java/com/newegg/ec/search/ms/ReactiveMsExampleApplication.java
+++ src/main/java/com/newegg/ec/search/ms/ReactiveMsExampleApplication.java
@@ -1,7 +1,9 @@
 package com.newegg.ec.search.ms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ReactiveMsExampleApplication {
 
     public static void main(String[] args) {
